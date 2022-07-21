145b0ad96d4e037a685beaaae7983d5d26f917a4
Nicklas <gitnicklas@pm.me>
2022-07-21 09:33:31 +0300

    Added camera control module

--- utility/cam_control.py
+++ utility/cam_control.py
@@ -1,43 +1,20 @@
 import os
-from time import perf_counter
 import sys
-import warnings
-import json
-from itertools import product
 from pathlib import Path
-from numba import njit, prange
 import cv2
-import math
 import torch
 import numpy as np
-from PIL import Image, ImageDraw
 import pyrealsense2 as rs
-from scipy import stats
-
-from ipdb import iex
-from matplotlib import pyplot as plt
 
 from os.path import join as pjoin
-from bop_toolkit_lib import inout
-warnings.filterwarnings("ignore")
 
 base_path = os.path.dirname(os.path.abspath("."))
 sys.path.append(base_path)
 
-from utility import timeit, load_segmentation_model
-from lib import (rendering, network, triangulate)
-
-from lib.render_cloud import load_cloud, render_cloud
-
-from dataset import LineMOD_Dataset, demo_dataset
-from evaluation import utils
-from evaluation import config as cfg
-
-DEVICE = torch.device('cuda')
-
-class Camera()
+class Camera():
 
     def __init__(self, size=(640, 480), framerate=60):
+
         self.pipeline = None
         self.config = None
         self.align = None
@@ -50,8 +27,8 @@ class Camera()
         self.config = rs.config()
         
         # Get device product line for setting a supporting resolution
-        pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-        pipeline_profile = config.resolve(pipeline_wrapper)
+        pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        pipeline_profile = self.config.resolve(pipeline_wrapper)
         device = pipeline_profile.get_device()
         device_product_line = str(device.get_info(rs.camera_info.product_line))
         
@@ -64,9 +41,9 @@ class Camera()
             print("The demo requires Depth camera with Color sensor")
             exit(0)
         
-        config.enable_stream(rs.stream.depth, size[0], size[1], rs.format.z16, framerate)
+        self.config.enable_stream(rs.stream.depth, size[0], size[1], rs.format.z16, framerate)
         #config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, size[0], size[1], rs.format.bgr8, framerate)
+        self.config.enable_stream(rs.stream.color, size[0], size[1], rs.format.bgr8, framerate)
         #config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     
         # Create an align object
@@ -81,7 +58,7 @@ class Camera()
         ### TODO: Get directly from device without capturing frame.
 
         # Start streaming
-        profile = pipeline.start(self.config)
+        profile = self.pipeline.start(self.config)
     
         # Getting the depth sensor's depth scale (see rs-align example for explanation)
         depth_sensor = profile.get_device().first_depth_sensor()
@@ -89,7 +66,7 @@ class Camera()
         print("Depth Scale is: " , self.depth_scale)
     
         # Get frameset of color and depth
-        frames = pipeline.wait_for_frames()
+        frames = self.pipeline.wait_for_frames()
         
         # Align the depth frame to color frame
         aligned_frames = self.align.process(frames)
@@ -108,44 +85,46 @@ class Camera()
                             [0, i.fy, i.ppy],
                             [0, 0,  1]], device='cpu') 
     
-        print("Camera intrinsics:", K)
+        print("Camera intrinsics:", self.cam_K)
     
-        return depth_scale, K, np.asanyarray(color_frame.get_data())
+    def get_rs_frame(self):
 
-        def get_frame(self):
-            # Get frameset of color and depth
-            frames = pipeline.wait_for_frames()
+        # Get frameset of color and depth
+        frames = self.pipeline.wait_for_frames()
 
-            # Align the depth frame to color frame
-            aligned_frames = align.process(frames)
+        # Align the depth frame to color frame
+        aligned_frames = self.align.process(frames)
 
-            # Get aligned frames
-            aligned_depth_frame = aligned_frames.get_depth_frame() 
-            color_frame = aligned_frames.get_color_frame()
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame() 
+        color_frame = aligned_frames.get_color_frame()
 
-            # Validate that both frames are valid
-            if not aligned_depth_frame or not color_frame:
-                return None
-            
-            return aligned_depth_frame, color_frame
+        # Validate that both frames are valid
+        if not aligned_depth_frame or not color_frame:
+            return None
+        
+        return aligned_depth_frame, color_frame
 
-        def get_image(self):
-            # Get frameset of color and depth
-            frames = pipeline.wait_for_frames()
-            # Align the depth frame to color frame
-            aligned_frames = align.process(frames)
-            # Get aligned frames
-            # aligned_depth_frame is a 640x480 depth image
-            aligned_depth_frame = aligned_frames.get_depth_frame() 
-            color_frame = aligned_frames.get_color_frame()
-            # Validate that both frames are valid
-            if not aligned_depth_frame or not color_frame:
-                return None
+    def get_image(self):
+        # Get frameset of color and depth
+        frames = self.pipeline.wait_for_frames()
+        # Align the depth frame to color frame
+        aligned_frames = self.align.process(frames)
+        # Get aligned frames
+        # aligned_depth_frame is a 640x480 depth image
+        aligned_depth_frame = aligned_frames.get_depth_frame() 
+        color_frame = aligned_frames.get_color_frame()
+        # Validate that both frames are valid
+        if not aligned_depth_frame or not color_frame:
+            return None
 
-            depth_image = np.asanyarray(aligned_depth_frame.get_data())
-            color_image = np.asanyarray(color_frame.get_data())
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
 
-            return depth_image, color_image
+        return depth_image, color_image
+
+    def __del__(self):
+            self.pipeline.stop()
 
     
 if __name__=="__main__":
@@ -156,51 +135,14 @@ if __name__=="__main__":
     try:
         while True:
 
-            depth_image, color_image
+            depth_image, color_image = cam.get_image()
 
-            #timeit.log("Pose estimation.")
-            mask, mask_gpu = segmentator(color_image)
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET) 
 
-            #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET) 
-            if mask.size != 0:
-
-                ### TODO: Can we get depth_image dircetly to gpu from sensor and skip gpu --> cpu with <mask>
-                R, t = pose_estimator.estimate_pose(obj_mask=mask_gpu,
-                            obj_depth=torch.tensor( (depth_image*mask*depth_scale).astype(np.float32)).squeeze())
-
-                #if count % args.buffer_size == 0:
-                #    count = -1
-                #else:
-                #    continue
-
-                #timeit.endlog()
-                #timeit.log("Rendering.")
-
-                color_image, done = dataset.render_cloud(obj_id=obj_id, 
-                        R=R.numpy().astype(np.float32), 
-                        t=t.numpy().astype(np.float32),
-                        image=color_image)
-
-                #color_image, done = dataset.render_mesh(obj_id=obj_id, 
-                #        R=R.numpy().astype(np.float32), 
-                #        t=t.numpy().astype(np.float32),
-                #        image=color_image)
-
-                # Point cloud matching failed.
-                if not done: continue
-
-                #import pdb; pdb.set_trace()
-                if args.render_mesh:
-                    pass
-
-                images = np.hstack([ 
-                    color_image, color_image*(mask[...,None]) ])
-                #timeit.endlog()
-            else:
-                images = np.hstack((color_image, color_image))
-
+            images = np.hstack((color_image, depth_colormap))
             cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
             cv2.imshow('Align Example', images)
+
             key = cv2.waitKey(1)
             # Press esc or 'q' to close the image window
             if key & 0xFF == ord('q') or key == 27:
@@ -208,5 +150,5 @@ if __name__=="__main__":
                 break
 
     finally:
-        pipeline.stop()
+        del cam
 
